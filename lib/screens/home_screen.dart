08c62deeae45235f8b7974cc7c823c7f2ee69ad9
saeedbm <iamsaeedbm@gmail.com>
2025-05-07 22:58:25 +0330

    modified home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,81 +17,114 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            height: 132,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.blueGrey,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  MSHCheckbox(
-                    size: 30,
-                    value: isChecked,
-                    colorConfig: MSHColorConfig.fromCheckedUncheckedDisabled(
-                      checkedColor: Colors.green,
-                    ),
-                    style: MSHCheckboxStyle.fillScaleCheck,
-                    onChanged: (selected) {
-                      setState(() {
-                        isChecked = selected;
-                      });
-                    },
-                  ),
-                  Spacer(),
-                  Column(
-                    children: [
-                      Text('data'),
-                      Text('data'),
-                      Row(
-                        children: [
-                          Container(
-                            width: 83,
-                            height: 28,
-                            decoration: BoxDecoration(
-                              color: Colors.greenAccent,
-                              borderRadius: BorderRadius.circular(18),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 12,
-                                vertical: 6,
-                              ),
-                              child: Row(
-                                children: [
-                                  Image.asset('images/icon_time.png'),
-                                  Text('time'),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Container(
-                            width: 83,
-                            height: 28,
-                            decoration: BoxDecoration(
-                              color: Colors.greenAccent[100],
-                              borderRadius: BorderRadius.circular(18),
-                            ),
-                          ),
-                        ],
+    return Scaffold(body: SafeArea(child: Center(child: getTaskItem())));
+  }
+
+  Container getTaskItem() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      height: 132,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.blueGrey,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: getMainContent(),
+      ),
+    );
+  }
+
+  Row getMainContent() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        MSHCheckbox(
+          size: 30,
+          value: isChecked,
+          colorConfig: MSHColorConfig.fromCheckedUncheckedDisabled(
+            checkedColor: Colors.green,
+          ),
+          style: MSHCheckboxStyle.fillScaleCheck,
+          onChanged: (selected) {
+            setState(() {
+              isChecked = selected;
+            });
+          },
+        ),
+        Spacer(),
+        getTimeAndEditBadgs(),
+        Image.asset('images/workout.png'),
+      ],
+    );
+  }
+
+  Column getTimeAndEditBadgs() {
+    return Column(
+      children: [
+        Text('data'),
+        Text('data'),
+        Row(
+          children: [
+            Container(
+              width: 90,
+              height: 30,
+              decoration: BoxDecoration(
+                color: Colors.greenAccent,
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 6,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      '22:22',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
-                  Image.asset('images/workout.png'),
-                ],
+                    ),
+                    SizedBox(width: 10),
+                    Image.asset('images/icon_time.png'),
+                  ],
+                ),
               ),
             ),
-          ),
+            SizedBox(width: 10),
+            Container(
+              width: 90,
+              height: 30,
+              decoration: BoxDecoration(
+                color: Colors.greenAccent[100],
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 6,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      'edit',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Image.asset('images/icon_edit.png'),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
+      ],
     );
   }
 }
